b72d934de5e79c7646f7e18530b8c4297dd60cb8
tecellicelik <tecelli.celik@gmail.com>
2018-01-16 15:06:01 +0300

    Create test.rs

---
+++ test.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // The statements here will be executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+}
