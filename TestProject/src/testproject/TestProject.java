86b41b2fb55bea02bec809558810b4f360aacc34
tecelli <tecelli@tecelli-VPCF23EFX>
2018-01-23 15:57:01 +0300

    yeni proje

---
+++ TestProject/src/testproject/TestProject.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testproject;
+
+/**
+ *
+ * @author tecelli
+ */
+public class TestProject {
+
+    int a, b;
+    TestProject(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    public int add() {
+        return (a + b);
+    }
+    
+}
---
+++ TestProject/test/testproject/TestProjectTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testproject;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tecelli
+ */
+public class TestProjectTest {
+    TestProject math;
+
+    @Before
+    public void setUp() {
+        math=new TestProject(6, 36);
+    }
+    
+    /**
+     * Test of add method, of class Math.
+     */
+    @Test
+    public void testAdd() {
+        Assert.assertEquals(42, math.add());
+    }
+    
+}
